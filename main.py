cbe4162e55efb3656af220dbe6a4100fe2eb7921
smphamba <samiemphamba@gmail.com>
2023-10-31 09:34:54 +0200

    renamed instance name from wGraph to wg

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 
 from weighted_graph import WGraph  # Import the WeightedGraph class
 
-# Create a weighted graph instance
-wGraph = WGraph()
 
-# Add nodes to the graph
-wGraph.insert_node("Dedza")
-wGraph.insert_node("Lilongwe")
-wGraph.insert_node("Dowa")
-wGraph.insert_node("Salima")
-wGraph.insert_node("Nkhotakota")
+districts = [
+    "Lilongwe",
+    "Dowa",
+    "Mchinji",
+    "Ntcheu",
+    "Dedza",
+    "Ntchisi",
+    "Kasungu",
+    "Salima",
+    "Nkhotakota"
+]
+
+# Create a weighted graph instance
+wg = WGraph()
+
+# Insert each district into the graph
+for district in districts:
+    wg.insert_node(district)
+
