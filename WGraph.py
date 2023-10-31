ae69c41708f88993431bf1d454a60b29a546da3f
smphamba <samiemphamba@gmail.com>
2023-10-31 06:51:24 +0200

    added insert node def

--- WGraph.py
+++ WGraph.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
+"""
+
+wGraph initializes a dictionary. 
+It has method to add node(district name), to the dictionary
+Also, you can add weight to the dictionary by associating two given nodes to the weight
+It then creates a weighted graph instance
+Lastly it calcultes shortest distance from A to B
+
+"""
+
 class wGraph:
 	def __init__(self):
 		self.graph = {} #   dictionary
-  
-  
-# echo "# weighted_graph" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/samiemphamba/weighted_graph.git
-# git push -u origin main
+	def insert_node(self, n):
+		# add new node to the dictinary if does not exits
+		# This assign district name a key to this node
+		if n not in self.graph:
+			self.graph[n] = {}
