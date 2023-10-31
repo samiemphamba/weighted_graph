272c37d13b8a7962cc2d5883254e0af5946538f0
smphamba <samiemphamba@gmail.com>
2023-10-31 07:05:27 +0200

    added insert edge def

--- WGraph.py
+++ WGraph.py
@@ -18,3 +18,14 @@ class wGraph:
 		# This assign district name a key to this node
 		if n not in self.graph:
 			self.graph[n] = {}
+	
+	def insert_edge(self, n1, n2, w):
+		# check if n1 and n2 are in graph.
+		# This will prevent us from adding null nodes
+		if n1 in self.graph and n2 in self.graph:
+			# From the graph, it looks undirected.
+			# So it is a must to associate the nodes in two possible directions
+			# From A to B = 10 or B to A = 10
+			self.graph[n1][n2] = w
+			self.graph[n2][n1] = w
+	
