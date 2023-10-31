d679b08b0258697322335eea2bb2dfc8be0a27c2
samson Mphamba <samiemphamba@gmail.com>
2023-10-31 06:35:09 +0200

    clearing git commands

--- WGraph.py
+++ WGraph.py
@@ -3,12 +3,3 @@
 class wGraph:
 	def __init__(self):
 		self.graph = {} #   dictionary
-  
-  
-# echo "# weighted_graph" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/samiemphamba/weighted_graph.git
-# git push -u origin main
